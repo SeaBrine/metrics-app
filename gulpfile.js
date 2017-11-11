b54732cf04061bc0b4b23cf380b9d0fb7031f1c6
Daniel Silva <silva20102@gmail.com>
2017-11-11 19:31:25 -0200

    Adicionado gulp e shelljs e incremento automático de versionamento

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,12 @@
 /**
  * Created by DanielSilva on 11/11/17.
  */
+
+var gulp = require('gulp');
+var shell = require('shelljs');
+
+gulp.task('version', function () {
+  shell.exec("npm version prerelease -f");
+});
+
+gulp.watch(['api/**/*.js', 'config/**/*.js', 'views/**/*.ejs'], ['version']);
