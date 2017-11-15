d2ed7c570cdc3577e72173831f3d51ffc646d9f2
Daniel Silva <silva20102@gmail.com>
2017-11-14 22:56:17 -0200

    Habilitado testes unitários no Sails.js

---
+++ test/integration/models/User.test.js
@@ -0,0 +1,18 @@
+/**
+ * Created by DanielSilva on 14/11/17.
+ */
+
+describe('UserModel', function() {
+
+  describe('#find()', function() {
+    it('should check find function', function (done) {
+      User.find()
+        .then(function(results) {
+          // some tests
+          done();
+        })
+        .catch(done);
+    });
+  });
+
+});
