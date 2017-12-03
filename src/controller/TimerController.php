57da82b5080f3ad7126c1aa2d787c43b0a1b7f90
Daniel Silva <silva20102@gmail.com>
2017-12-03 18:22:52 -0200

    Adicionado classe TimerController

---
+++ src/controller/TimerController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DanielSilva
+ * Date: 03/12/17
+ * Time: 18:20
+ */
+
+namespace Metrics\Controller;
+
+
+class TimerController extends Controller
+{
+    private $login;
+    private $loginModel;
+    private $timerModel;
+}
